eeefafc21962dbbc440b0935a2c647cdee79cac7
Exoldarium <shandoo91@gmail.com>
2025-05-05 14:54:07 +0700

    login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,68 @@
-import { Link } from '@heroui/link';
+'use client';
 
-function Login() {
+import { Link } from '@heroui/link';
+import { Button, Form, Input } from '@heroui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch('email'));
+
   return (
     <div className="flex flex-col justify-center items-center">
       <Link href="/">Fake Store</Link>
-      <p>this is a login page</p>
+      <Form
+        className="w-full max-w-xs flex flex-col gap-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input
+          isRequired
+          errorMessage="Please enter a valid email address"
+          label="E-mail"
+          labelPlacement="outside"
+          placeholder="Enter your email"
+          type="email"
+          {...register('email')}
+        />
+
+        <Input
+          isRequired
+          errorMessage={(value) => value.validationErrors[0]}
+          label="Password"
+          labelPlacement="outside"
+          placeholder="Enter your password"
+          type="password"
+          {...register('password')}
+          validate={(value) => {
+            if (value.length < 5) {
+              return 'Password must be at least 5 characters long';
+            }
+          }}
+        />
+        <div className="flex gap-2">
+          <Button color="primary" type="submit">
+            Submit
+          </Button>
+          <Button type="reset" variant="flat">
+            Reset
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 }
 
-export default Login;
+export default LoginPage;
